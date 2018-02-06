774057f05ee54d29fd93c5ccb9e70d0518d93beb
SouthernRebs <fordviper2012@yahoo.com>
2018-02-05 23:48:52 -0600

    Added challenges from chapters 5 & 6 from Big Nerd Ranch. Added "cheat" button as well as added a view that allows multiple cheats and a view that reports the version API level.

--- app/src/main/java/com/example/fordv/fordquizchallenges/MainActivity.java
+++ app/src/main/java/com/example/fordv/fordquizchallenges/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fordv.fordquizchallenges;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,22 +14,29 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
+    private TextView mCheatTextView;
+    private TextView mApiLevelTextView;
     private ArrayList<Integer> mQuestionAsked = new ArrayList<Integer>(6);
     private int mTrueAnswer = 0;
+    public static final String KEY_CHEATER = "cheater";
 
-    private Question[] mQuestionBank = new Question[] {
+    private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -36,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private int mCurrentIndex = 0;
+    int maxCheats = 3;
+    int mNumberOfCheats = 0;
+    private boolean mIsCheater = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
             mQuestionAsked = savedInstanceState.getIntegerArrayList("myArray");
         }
 
+        mApiLevelTextView = (TextView) findViewById(R.id.current_api_level_text_view);
+        mApiLevelTextView.setText("API Level is " + Build.VERSION.SDK_INT);
+        mCheatTextView = (TextView) findViewById(R.id.cheat_text_view);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 updateQuestion();
 
 
@@ -88,12 +105,39 @@ public class MainActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                int length = mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex + (length - 1)) % length;
+                mIsCheater = false;
                 updateQuestion();
             }
         });
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNumberOfCheats++;
+                if(mNumberOfCheats >= maxCheats){
+                    mCheatTextView.setText(R.string.too_many_cheats_toast);
+                    mCheatButton.setVisibility(View.INVISIBLE);
+                }
+                updateCheatView();
+
+                Intent intent = new Intent(MainActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                intent.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+                startActivityForResult(intent, 0);
+            }
+        });
         updateQuestion();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+        }
     }
 
     @Override
@@ -119,7 +163,12 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-        savedInstanceState.putIntegerArrayList("myArray", mQuestionAsked);
+        savedInstanceState.putBoolean(KEY_CHEATER, mIsCheater);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+            mIsCheater = savedInstanceState.getBoolean(KEY_CHEATER, false);
+        }
     }
 
     @Override
@@ -132,6 +181,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy() called");
+    }
+
+    private void updateCheatView(){
+        mCheatTextView = (TextView) findViewById(R.id.cheat_text_view);
+        mCheatTextView.setText("You have used " + mNumberOfCheats + " cheat(s),  " + (maxCheats - mNumberOfCheats) + "  left.");
     }
 
     private void updateQuestion() {
@@ -147,9 +201,9 @@ public class MainActivity extends AppCompatActivity {
                 mTrueButton.setVisibility(View.INVISIBLE);
             }
         }
-        int resultResId = (mTrueAnswer*100) / 6;
+        int resultResId = (mTrueAnswer * 100) / 6;
         if (mQuestionAsked.size() > 5) {
-            Toast.makeText(this, Integer.toString(resultResId)+"% correct answers", Toast.LENGTH_LONG)
+            Toast.makeText(this, Integer.toString(resultResId) + "% correct answers", Toast.LENGTH_LONG)
                     .show();
         }
     }
@@ -174,15 +228,22 @@ public class MainActivity extends AppCompatActivity {
 
         int messageResId = 0;
 
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
-            mTrueAnswer = mTrueAnswer + 1;
+        if (mIsCheater) {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.judgment_toast;
+//            } else {
+//                messageResId = R.string.incorrect_judgement_toast;
+            }
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
 
-        Toast toast = Toast.makeText(MainActivity.this,messageResId,Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP,0,200);
+        Toast toast = Toast.makeText(MainActivity.this, messageResId, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, 200);
         toast.show();
     }
 }
